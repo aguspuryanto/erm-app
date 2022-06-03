e1f42fe7165927c718f6ce962e27ac70f7fd86de
Agus Puryanto <aguspuryanto@gmail.com>
2022-06-03 12:49:45 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dxn2u.erm',
+  appName: 'ERMApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
